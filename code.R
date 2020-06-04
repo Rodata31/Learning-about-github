647890734f91f9d2fdbabcd6a8593709058cbb75
Rodrigo Cerna <rod9231@gmail.com>
2020-06-04 18:05:23 -0500

    Adding labels and title to the plot

--- code.R
+++ code.R
@@ -4,4 +4,7 @@ data(murders)
 
 ggplot(murders,aes(population,total,label = abb, fill = region)) + 
   geom_point() + 
-  geom_label()
+  geom_label() + 
+  ggtitle("Population and total murders in US - 2010") +
+  xlab("Population (in millions)") + 
+  ylab("total murders")
